961c5d41bdb5d45c04d1ff71564a950f3be3c932
gonghuijue <>
2023-05-27 16:13:47 +0800

    style: 颜色配置调整

---
+++ src/constants/theme.ts
@@ -0,0 +1,9 @@
+import { ThemeConfig } from "antd";
+
+export const themeConfig:ThemeConfig["token"] = {
+  colorPrimary:'#9cada1',
+  colorSuccess:'#b1c0bf',
+  colorError:'#90655a',
+  colorWarning:'#c9957a',
+  colorInfo:'#cdd8cc'
+}
