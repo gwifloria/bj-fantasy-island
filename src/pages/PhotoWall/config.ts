25686d4f4be8df693b4f12b941c0cd58a62c3300
gonghuijue <gonghuijue@senseauto.com>
2024-02-18 14:10:52 +0800

    chore: change pic

--- src/pages/PhotoWall/config.ts
+++ src/pages/PhotoWall/config.ts
@@ -1,17 +1,12 @@
-import phuket from "@/assets/phuket.jpg";
-import phuket1 from "@/assets/phuket1.jpg";
-import phuket2 from "@/assets/phuket2.jpg";
-import phuket3 from "@/assets/phuket3.jpg";
+import automobile from "@/assets/automobile.png";
+import h5 from "@/assets/h5.png";
+
 import { AlbumType } from "./type";
 
 export const albumsConfig:{key:AlbumType,items:string[]}[] = [
   {
     key:'portrait',
-    items:[phuket,phuket1,phuket2,phuket3]
-  },
-  {
-    key:'trip',
-    items:[phuket2,phuket3]
+    items:[automobile,h5]
   },
   
 ]
